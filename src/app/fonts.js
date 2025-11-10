577167c04ab11573700009968b331010a9abd2ec
Fahad9872 <kazimohammadfahad@gmail.com>
2025-11-10 11:47:11 +0600

    hero section

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -18,9 +18,30 @@ export const geistMono = Geist_Mono({
 export const integralCF = localFont({
   src: [
     {
-      path: "../../public/fonts/IntegralCF-Regular.otf", // ✅ relative path to public folder
+      path: "../../public/fonts/IntegralCF-Regular.otf",
       weight: "400",
-      style: "normal",
+      style: "normal", // Corrected
+    },
+    {
+      path: "../../public/fonts/integralcf-medium.otf",
+      weight: "500",
+      style: "normal", // Corrected
+    },
+    {
+      path: "../../public/fonts/integralcf-demibold.otf",
+      weight: "600",
+      style: "normal", // Corrected
+    },
+    {
+      path: "../../public/fonts/integralcf-bold.otf",
+      weight: "700",
+      style: "normal", // Corrected
+    },
+    {
+      // Assuming this file is the italic/oblique version
+      path: "../../public/fonts/integralcf-extraboldoblique.otf",
+      weight: "900",
+      style: "italic", // Must be 'italic' or 'oblique' for the slanted file
     },
   ],
   variable: "--font-integral",
@@ -32,22 +53,22 @@ export const satoshi = localFont({
     {
       path: "../../public/fonts/Satoshi-Regular.otf",
       weight: "400",
-      style: "normal",
+      style: "normal", // Corrected
     },
     {
       path: "../../public/fonts/Satoshi-Light.otf",
       weight: "300",
-      style: "normal",
+      style: "normal", // Corrected
     },
     {
       path: "../../public/fonts/Satoshi-Medium.otf",
       weight: "500",
-      style: "normal",
+      style: "normal", // Corrected
     },
     {
       path: "../../public/fonts/Satoshi-Bold.otf",
       weight: "700",
-      style: "normal",
+      style: "normal", // Corrected
     },
   ],
   variable: "--font-satoshi",
